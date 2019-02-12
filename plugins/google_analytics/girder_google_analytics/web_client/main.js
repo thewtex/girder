6d3ff66bb0735126f797aaf0f1214fecf15e3411
Matt McCormick <matt.mccormick@kitware.com>
2019-02-12 14:50:24 -0500

    Google Analytics plugin respects Do Not Track
    Do Not Track is sent from an individual's web browser to request that their activity is not tracked:
    
      https://allaboutdnt.com/
    
    Respect Do Not Track in the Google Analytics plugin as suggested:
    
      https://www.paulfurley.com/google-analytics-do-not-track/

--- plugins/google_analytics/girder_google_analytics/web_client/main.js
+++ plugins/google_analytics/girder_google_analytics/web_client/main.js
@@ -5,21 +5,26 @@ import './lib/backbone.analytics';
 import './routes';
 
 events.on('g:appload.after', function () {
+    var dnt = navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack;
     /* eslint-disable */
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    /* eslint-enable */
+    if (dnt != "1" && dnt != "yes") {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        /* eslint-enable */
 
-    restRequest({
-        method: 'GET',
-        url: 'google_analytics/id'
-    }).done((resp) => {
-        if (resp.google_analytics_id) {
-            ga('create', resp.google_analytics_id, 'none');
-        }
-    });
+        restRequest({
+            method: 'GET',
+            url: 'google_analytics/id'
+      }).done((resp) => {
+            if (resp.google_analytics_id) {
+                ga('create', resp.google_analytics_id, 'none');
+            }
+      });
+    else {
+        console.debug("Respecting Do-Not-Track, not loading analytics. See https://www.paulfurley.com/google-analytics-dnt/");
+    }
 });
 
 /**
